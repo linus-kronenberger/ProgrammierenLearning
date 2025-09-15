2a72db2b7c70dabf67db79d4cb26a8dd53d7dbd4
I749983 <linus.kronenberger@sap.com>
2025-09-15 19:05:38 +0200

    thread template runnable, time control

--- de/dhbwka/java/templates/Threads/ThreadImplemented.java
+++ de/dhbwka/java/templates/Threads/ThreadImplemented.java
@@ -1,5 +1,16 @@
 package de.dhbwka.java.templates.Threads;
 
-public class ThreadImplemented {
-    
+public class ThreadImplemented implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("my thread works!!!");
+    }
+    public static void main(String[] args) {
+        ThreadImplemented myThread = new ThreadImplemented();
+        ThreadImplemented myThread2 = new ThreadImplemented();
+        Thread thread1 = new Thread(myThread);
+        Thread thread2 = new Thread(myThread2);
+        thread1.start();
+        thread2.start();
+    }
 }
---
+++ de/dhbwka/java/templates/Threads/ThreadTime.java
@@ -0,0 +1,35 @@
+package de.dhbwka.java.templates.Threads;
+
+public class ThreadTime extends Thread {
+    private long time = System.currentTimeMillis();
+    private int threadNumber;
+    public ThreadTime(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
+    @Override
+    public void run() {
+        try {
+            if(this.threadNumber == 2) {
+                Thread.sleep(500);
+                time = System.currentTimeMillis();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        while(true) {
+            long newTime = System.currentTimeMillis();
+            if(newTime - time > 1000) {
+                System.out.println("thread number: " + this.threadNumber + " peeps!!");
+                time = System.currentTimeMillis();
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        ThreadTime thread1 = new ThreadTime(1);
+        ThreadTime thread2 = new ThreadTime(2);
+        thread1.start();
+        thread2.start();
+    }
+}
