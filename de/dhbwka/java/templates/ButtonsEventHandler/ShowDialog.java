4353f7c5bf8a9a7cfb2ee491abc21282b0ff8b84
I749983 <linus.kronenberger@sap.com>
2025-09-23 01:56:44 +0200

    template jdialog

---
+++ de/dhbwka/java/templates/ButtonsEventHandler/ShowDialog.java
@@ -0,0 +1,16 @@
+package de.dhbwka.java.templates.ButtonsEventHandler;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ShowDialog extends JFrame{
+    public static void main(String[] args) {
+        ShowDialog frame = new ShowDialog();
+        JDialog dialog = new JDialog(frame, "hi from my dialog!!!");
+        JLabel labelForDialog = new JLabel("hey from my label");
+        dialog.add(labelForDialog);
+        dialog.setVisible(true);
+        frame.setVisible(true);
+    }
+}
